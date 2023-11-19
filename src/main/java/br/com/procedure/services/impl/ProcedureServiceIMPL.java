dce8dbe403605e981b13e4803a25cd1a4a34f492
Arthur Neves <arthurneves@pop-os.localdomain>
2023-11-19 14:10:50 -0300

    ACHO QUE ESTÀ TERMINADO

--- src/main/java/br/com/procedure/services/impl/ProcedureServiceIMPL.java
+++ src/main/java/br/com/procedure/services/impl/ProcedureServiceIMPL.java
@@ -1,14 +1,15 @@
 package br.com.procedure.services.impl;
 
-import br.com.procedure.dtos.ProcedureCreateRequest;
-import br.com.procedure.dtos.ProcedureResponse;
-import br.com.procedure.dtos.ProcedureUpdateRequest;
+import br.com.procedure.dtos.*;
 import br.com.procedure.entities.Procedure;
 import br.com.procedure.repository.ProcedureRepository;
 import br.com.procedure.services.ProcedureService;
 import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.Condition;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.stereotype.Service;
 
 
@@ -25,11 +26,15 @@ public class ProcedureServiceIMPL implements ProcedureService {
     private final ArrayList<ProcedureResponse> procedureResponseList;
     @Override
     public ProcedureResponse create(ProcedureCreateRequest procedureRequest){
-        Procedure procedure = modelMapper.map(procedureRequest,Procedure.class);
-        repository.insert(procedure);
-        ProcedureResponse procedureResponse = modelMapper.map(procedure,ProcedureResponse.class);
-        return procedureResponse;
-
+        try {
+            Procedure procedure = modelMapper.map(procedureRequest, Procedure.class);
+            repository.insert(procedure);
+            ProcedureResponse procedureResponse = modelMapper.map(procedure, ProcedureResponse.class);
+            return procedureResponse;
+        }
+        catch (Exception e){
+            throw new ValidationException(e);
+        }
     }
     @Override
     public void delete(String id){
@@ -38,26 +43,44 @@ public class ProcedureServiceIMPL implements ProcedureService {
 
     @Override
     public List<ProcedureResponse> findAllProcedures() {
-        repository.findAll().forEach((procedure)->{
-            ProcedureResponse procedureResponse = modelMapper.map(procedure,ProcedureResponse.class);
-            procedureResponseList.add(procedureResponse);
-        });
-        return procedureResponseList;
+        try{
+            repository.findAll().forEach((procedure)->{
+                ProcedureResponse procedureResponse = modelMapper.map(procedure,ProcedureResponse.class);
+                procedureResponseList.add(procedureResponse);
+            });
+            return procedureResponseList;
+        }
+        catch (Exception e){
+            throw new ValidationException(e);
+        }
     }
 
     @Override
     public ProcedureResponse update(ProcedureUpdateRequest procedureUpdateRequest){
-        modelMapper.getConfiguration().setSkipNullEnabled(true);
-        Procedure procedure = repository.findById(procedureUpdateRequest.getId()).orElseThrow(()->new ValidationException("Procedure not found"));
-        modelMapper.map(procedureUpdateRequest,procedure);
-        repository.save(procedure);
-        return modelMapper.map(procedure,ProcedureResponse.class);
+        try {
+            modelMapper.getConfiguration().setSkipNullEnabled(true).setDeepCopyEnabled(true);
+            Procedure procedure = repository.findById(procedureUpdateRequest.getId()).orElseThrow(()->new ValidationException("Procedure not found"));
+            if(procedureUpdateRequest.getSessions()!=null){
+                procedure.setSessions(null);
+            }
+            modelMapper.map(procedureUpdateRequest,procedure);
+            repository.save(procedure);
+            return modelMapper.map(procedure,                                                                                                                                                           ProcedureResponse.class);
+        }
+        catch (Exception e){
+            throw new ValidationException(e);
+        }
     }
     @Override
     public ProcedureResponse findById(String id){
-        Procedure procedure= repository.findById(id).get();
-        ProcedureResponse procedureResponse = modelMapper.map(procedure,ProcedureResponse.class);
-        return procedureResponse;
+        try {
+            Procedure procedure= repository.findById(id).orElseThrow(()-> new ValidationException("Procedure not found"));
+            ProcedureResponse procedureResponse = modelMapper.map(procedure,ProcedureResponse.class);
+            return procedureResponse;
+        }
+        catch (Exception e){
+            throw new ValidationException(e);
+        }
     }
 
 }
